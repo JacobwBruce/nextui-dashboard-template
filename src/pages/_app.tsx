4ffc56be6d95ea428cfeba0c7bf56896a150b291
Jacob Bruce <jacobwbruce@hotmail.com>
2023-10-17 16:46:24 -0400

    NextUI Installed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { type AppType } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { Inter } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+const inter = Inter({ subsets: ["latin"] });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </NextUIProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
